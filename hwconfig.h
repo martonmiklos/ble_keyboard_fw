9abf8b088a69375b1848b9a2a7e3133d698e8294
Miklós Márton <martonmiklosqdev@gmail.com>
2018-09-21 23:58:47 +0200

    Save hwconfig.h

---
+++ hwconfig.h
@@ -0,0 +1,42 @@
+#ifndef HWCONFIG_H
+#define HWCONFIG_H
+
+//-----------------------------------------------------------------
+//------------------------- DO NOT MODIFY -------------------------
+//------------------Generated with the generate_hearer.ulp---------
+//-----------------------------------------------------------------
+
+
+#define BACKLIGHT_PWM   24
+#define I2C_SCL   21
+#define I2C_SDA   22
+#define KSI0   18
+#define KSI1   19
+#define KSI2   16
+#define KSI3   20
+#define KSI4   10
+#define KSI5   0
+#define KSI6   6
+#define KSI7   5
+#define KSO0   11
+#define KSO1   13
+#define KSO2   17
+#define KSO3   14
+#define KSO4   15
+#define KSO5   0
+#define KSO6   7
+#define KSO7   4
+#define KSO8   3
+#define KSO9   2
+#define KSO10   1
+#define KSO11   0
+#define KSO12   30
+#define KSO13   29
+#define KSO14   28
+#define KSO15   27
+#define LED_CAPS   26
+#define LED_FN   25
+#define NENABLE_TOUCHPAD   12
+#define TOUCH_ALERT   23
+
+#endif //#define HWCONFIG_H
